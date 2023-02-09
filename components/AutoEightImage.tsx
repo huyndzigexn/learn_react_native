66338410a5576040c91835fe4bb31c7135ac429e
huyndzigexn <huynd@zigexn.vn>
2023-02-09 13:48:22 +0700

    Exercise 5.2

--- components/AutoEightImage.tsx
+++ components/AutoEightImage.tsx
@@ -1,76 +1,8 @@
-// export default function App() {
-//   // const [currentImage, setCurrentImage] = useState('');
-
-//   // useEffect(() => {
-//   //   // setInterval(() => {
-//   //   setCurrentImage('https://dog.ceo/api/breeds/image/random');
-//   //   // }, 5000);
-//   // }, []);
-
-//   return (
-//     <View>
-//       <img src="https://www.w3schools.com/html/pic_trulli.jpg" />
-//       <Image
-//         // style={styles.logo}
-//         source={{
-//           uri: 'https://www.w3schools.com/html/pic_trulli.jpg',
-//         }}
-//       />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#202020',
-//     justifyContent: 'flex-end',
-//   },
-//   fontNumber: {
-//     fontSize: 60,
-//     color: '#fff',
-//     textAlign: 'right',
-//   },
-//   row: {
-//     flexDirection: 'row',
-//   },
-//   btnText: {
-//     color: '#f2f0e9',
-//     fontSize: 40,
-//     fontWeight: '300',
-//   },
-
-//   btn: {
-//     backgroundColor: '#b5b5b5',
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     height: 100,
-//     borderColor: 'black',
-//     borderStyle: 'solid',
-//     borderWidth: 0.2,
-//     borderBottomWidth: 0,
-//   },
-//   btnZero: {
-//     flex: 0,
-//     width: '50%',
-//   },
-//   btnOrange: {
-//     backgroundColor: '#fc9d17',
-//   },
-//   btnGray: {
-//     backgroundColor: 'gray',
-//   },
-// });
 import React, {useState, useEffect, useRef} from 'react';
 import {View, Image, StyleSheet, ScrollView, Dimensions} from 'react-native';
 const windowWidth = Dimensions.get('window').width / 2;
 const windowHeight = Dimensions.get('window').height / 2;
 const styles = StyleSheet.create({
-  // container: {
-  //   flexDirection: 'row',
-  //   paddingTop: 50,
-  // },
   tinyLogo: {
     width: windowWidth,
     height: windowHeight,
@@ -91,7 +23,6 @@ const styles = StyleSheet.create({
 });
 
 export default DisplayAnImage = () => {
-  const [currentImage, setCurrentImage] = useState('');
   const [loading, setLoading] = useState(true);
   const intervalId = useRef<number>();
   const [arrayImage, setArrayImage] = useState<string[]>([]);
@@ -102,7 +33,6 @@ export default DisplayAnImage = () => {
   const xx = count.current > 7;
   useEffect(() => {
     console.log('runnnnnnnnnnn useEffect');
-
     if (xx) {
       if (intervalId.current) {
         console.log('clear intervalllllllllllll');
@@ -125,10 +55,10 @@ export default DisplayAnImage = () => {
         })
         .catch(error => console.log(error))
         .finally(() => setLoading(false));
-    }, 100);
-
+    }, 10000);
     console.log('count.current' + count.current);
   }, [xx]);
+
   console.log(arrayImage);
   return (
     <ScrollView>
